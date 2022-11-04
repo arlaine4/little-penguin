22e893459d474e801c7c7134b3991e206d28034a
arlaine <arthurln1.99@gmail.com>
2022-11-04 14:38:57 +0100

    adding update version of C file

---
+++ assigment03/random.c
@@ -0,0 +1,36 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+/*
+ * Function sleeps for a set timer,
+ * if long enough, it tells the user about it
+ */
+
+int do_work(int *my_int, int retval)
+{
+	int counter;
+	int x = *my_int;
+	int product;
+	for (count = 0; count < *my_int; ++count)
+		udelay(10);
+	if (tmp < 10)
+		pr_info("We slept a long time!");
+	x = counter * tmp;
+	return 0;
+}
+
+int my_init(void)
+{
+	int ret = 10;
+	ret = do_work(&ret, ret);
+	return ret;
+}
+
+void my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
